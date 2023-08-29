0fbe5129db2c118a669e9e3a5d460097e6c49d0b
JungHee Lee <36289819+MarongHappy@users.noreply.github.com>
2023-08-29 13:59:56 +0900

    Create sqreenshot_ko_KR.ts

---
+++ resources/translations/sqreenshot_ko_KR.ts
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KR">
+<context>
+    <name>DialogMain</name>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="38"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="153"/>
+        <source>Region</source>
+        <translation>캡처 영역</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="72"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="154"/>
+        <source>Options</source>
+        <translation>옵션</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="112"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="158"/>
+        <source>Window title and frame</source>
+        <oldsource>Include window title and frame</oldsource>
+        <translation>창 제목 및 프레임</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="158"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="159"/>
+        <source>&amp;OK</source>
+        <translation>확인(&amp;O)</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="105"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="157"/>
+        <source>Show mouse cursor</source>
+        <oldsource>Include mouse cursor</oldsource>
+        <translation>마우스 커서 표시하기</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="78"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="155"/>
+        <source>Delay:</source>
+        <translation>딜레이:</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.ui" line="85"/>
+        <location filename="../../build/x86-linux-generic-elf-64bit/debug/Sqreenshot_autogen/include/ui_dialogmain.h" line="156"/>
+        <source> sec</source>
+        <translation> 초</translation>
+    </message>
+</context>
+<context>
+    <name>azd::DialogMain</name>
+    <message>
+        <location filename="../../src/dialogmain.cpp" line="58"/>
+        <source>Screen</source>
+        <translation>화면</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.cpp" line="59"/>
+        <source>Window</source>
+        <translation>창</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.cpp" line="60"/>
+        <source>Selection</source>
+        <translation>선택</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.cpp" line="154"/>
+        <source>Error loading about file</source>
+        <translation>정보 파일을 불러올 수 없습니다</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogmain.cpp" line="160"/>
+        <source>About</source>
+        <translation>정보</translation>
+    </message>
+</context>
+<context>
+    <name>azd::DialogScreenShot</name>
+    <message>
+        <location filename="../../src/dialogscreenshot.cpp" line="79"/>
+        <source>Save to clipboard</source>
+        <translation>클립보드에 저장하기</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogscreenshot.cpp" line="120"/>
+        <source>Screenshot</source>
+        <translation>스크린숏</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogscreenshot.cpp" line="127"/>
+        <source>Save as</source>
+        <translation>다른 이름으로 저장</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogscreenshot.cpp" line="140"/>
+        <source>Error</source>
+        <translation>오류</translation>
+    </message>
+    <message>
+        <location filename="../../src/dialogscreenshot.cpp" line="141"/>
+        <source>Can&apos;t save the image: &quot;%1&quot;.</source>
+        <translation>이미지를 저장할 수 없습니다: &quot;%1&quot;.</translation>
+    </message>
+</context>
+<context>
+    <name>azd::DialogSelectionArea</name>
+    <message>
+        <location filename="../../src/dialogselectionarea.cpp" line="86"/>
+        <source>Press &quot;Esc&quot; key or left click to exit.</source>
+        <translation>종료하려면 &quot;Esc&quot; 키를 누르거나 마우스 왼쪽 버튼을 클릭하세요.</translation>
+    </message>
+</context>
+</TS>
